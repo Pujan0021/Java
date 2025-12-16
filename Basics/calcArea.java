9490acfa1711843325dddc5d968db04baecdf71a
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-16 08:37:39 +0545

    Comparison of Rectangle

--- Basics/calcArea.java
+++ Basics/calcArea.java
@@ -3,8 +3,12 @@ class Rectangle {
 
     int length, breadth;
 
-    int area() {
+    int computeArea() {
         return length * breadth;
+    }
+
+    void displayArea() {
+        System.out.println("The area  are :   " + computeArea());
     }
 
     Rectangle(int length, int breadth) {
@@ -17,7 +21,17 @@ class Rectangle {
 public class calcArea {
 
     public static void main(String[] args) {
-        Rectangle R1 = new Rectangle(10, 10);
-        System.out.println("Area: " + R1.area());
+        Rectangle R1 = new Rectangle(300, 10);
+        Rectangle R2 = new Rectangle(100, 20);
+        R1.displayArea();
+        R2.displayArea();
+        int areaOfR1 = R1.computeArea();
+        int areaOfR2 = R2.computeArea();
+
+        if (areaOfR1 > areaOfR2) {
+            System.out.println("The area of R1 is greater than R2. Area: " + areaOfR1);
+        } else {
+            System.out.println("The area of R2 is greater than R1. Area: " + areaOfR2);
+        }
     }
 }
