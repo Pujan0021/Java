c052d366ac0484cc1768b6dfc26c3fe55278938f
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-24 09:13:21 +0545

    Using Parent Method Using Super Keyword

--- Basics/useOfSuper.java
+++ Basics/useOfSuper.java
@@ -15,21 +15,25 @@ class duke extends motorBike {
 
     float mileage = 20.44f;
     String speed = "300k/h";
-    String company = "KTM";
+    String name = "KTM390 Adventure";
+
+    void display() {
+        super.specs();
+    }
 
     String company() {
-        System.out.println("Company of motorBike:");
+        System.out.println("Company of motorBike: " + company);
         return company;
     }
 
     float mileage() {
-        System.out.println("Mileage of Duke:");
+        System.out.println("Mileage of Duke: " + mileage);
         return mileage;
 
     }
 
     String speed() {
-        System.out.println("Speed of motorBike:");
+        System.out.println("Speed of motorBike:   " + super.speed);
 
         return super.speed;
     }
@@ -42,9 +46,10 @@ public class useOfSuper {
         // motorBike m1 = new motorBike();
         // System.out.println("Company:" + m1.specs());
         duke d1 = new duke();
-        System.out.println("Company:" + d1.company());
-        System.out.println("Mileage:" + d1.mileage());
-        System.out.println("Speed:" + d1.speed());
+        d1.display();
+        d1.company();
+        d1.mileage();
+        d1.speed();
 
     }
 }
