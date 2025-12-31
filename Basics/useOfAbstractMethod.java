0d10cf3bf039d7cdf15fd5d1656aba1297526ade
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-31 08:36:43 +0545

    Abstract Class

---
+++ Basics/useOfAbstractMethod.java
@@ -0,0 +1,31 @@
+
+abstract class Animal {
+
+    abstract void makesSound();
+
+}
+
+class Dog extends Animal {
+
+    void makesSound() {
+        System.out.println("BHauuuuuuuuuuuuuuuuuuuuu!");
+    }
+;
+
+}
+class Cat extends Animal {
+
+    void makesSound() {
+        System.out.println("Meow Meowwwwwwwwwwwwwww!");
+    }
+}
+
+public class useOfAbstractMethod {
+
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        Cat c1 = new Cat();
+        d1.makesSound();
+        c1.makesSound();
+    }
+}
