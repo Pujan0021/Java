564ee905fc0bfe1c5d5933492f660a8b127e80b3
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-25 08:50:04 +0545

    Gui

--- GUI/GuiApp.java
+++ GUI/GuiApp.java
@@ -1,20 +1,33 @@
 
-import java.awt.Color;
 import javax.swing.*;
 
 public class GuiApp {
 
     public static void main(String[] args) {
-        JFrame f = new JFrame("JFrame Example");
+        JFrame f = new JFrame("JFarame Examples");
+
+        JLabel f1 = new JLabel("Name : ");
+        f1.setBounds(10, 10, 50, 10);
+        f.add(f1);
+
+        JTextField jf = new JTextField();
+        jf.setBounds(70, 10, 100, 20);
+        f.add(jf);
+
+        JLabel f2 = new JLabel("Address : ");
+        f2.setBounds(10, 40, 50, 10);
+        f.add(f2);
+
+        JTextField jg = new JTextField();
+        jg.setBounds(70, 40, 100, 20);
+        f.add(jg);
+
+        JButton jb = new JButton("Sumit");
+        jb.setBounds(80, 70, 80, 20);
+        f.add(jb);
+
         f.setSize(500, 500);
         f.setLayout(null);
         f.setVisible(true);
-        JLabel l = new JLabel("Name: ");
-        l.setSize(30, 30);
-        l.setBounds(30, 40, 50, 50);
-        l.setBackground(Color.BLACK);
-        l.setVisible(true);
-        l.setLayout(null);
-        f.add(l);
     }
 }
